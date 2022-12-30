7d8a47279538bc9496c94cb9ae3b5c0d47acc75f
Hà Trọng Tấn <hatrongtan99@gmail.com>
2022-12-30 11:09:14 +0700

    config db

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,32 +1,32 @@
 const mysql = require('mysql2/promise');
 
 const config = {
-  host: 'localhost',
-  user: 'root',
-  database: 'db_web',
-  password: process.env.PASSWORD_MYSQL
+    host: 'localhost',
+    user: 'root',
+    database: 'db_web',
+    password: process.env.PASSWORD_MYSQL,
 };
 
 const execute = async (sql, params) => {
-  try {
-    const connection = await mysql.createConnection(config);
-    const [results, ] = await connection.execute(sql, params);
-    return results;
-
-  } catch (error) {
-    console.log('Failued when connect database ' + error)
-  }
-}
+    try {
+        const connection = await mysql.createConnection(config);
+        const [results] = await connection.execute(sql, params);
+        connection.end();
+        return results;
+    } catch (error) {
+        console.log('Failued when connect database ' + error);
+    }
+};
 
 const query = async (sql, params) => {
-  try {
-    const connection = await mysql.createConnection(config);
-    const [results, ] = await connection.query(sql, params);
-    return results;
+    try {
+        const connection = await mysql.createConnection(config);
+        const [results] = await connection.query(sql, params);
+        connection.end();
+        return results;
+    } catch (error) {
+        console.log('Failued when connect database ' + error);
+    }
+};
 
-  } catch (error) {
-    console.log('Failued when connect database ' + error)
-  }
-}
-
-module.exports = {execute, query}
+module.exports = { execute, query };
