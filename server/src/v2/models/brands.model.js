e6e73c267a07ee00658092b8dbe2eeaac47930b8
hatrongtan99 <hatrongtan99@gmail.com>
2023-02-15 21:31:53 +0700

    add descript brand

--- server/src/v2/models/brands.model.js
+++ server/src/v2/models/brands.model.js
@@ -1,39 +1,40 @@
-const { Schema, model } = require('mongoose');
-const slugify = require('slugify');
+const { Schema, model } = require("mongoose");
+const slugify = require("slugify");
 
 const brandSchema = new Schema(
-    {
-        brand_name: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        brand_thumb: {
-            type: String,
-            required: true,
-        },
-        isActive: {
-            type: Boolean,
-            default: true,
-        },
-        created: {
-            type: Date,
-            default: Date.now(),
-        },
-        updated: Date,
-        slug: {
-            type: String,
-            unique: true,
-        },
+  {
+    brand_name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { collection: 'Brands' }
+    brand_thumb: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    created: {
+      type: Date,
+      default: Date.now(),
+    },
+    updated: Date,
+    slug: {
+      type: String,
+      unique: true,
+    },
+  },
+  { collection: "Brands" }
 );
 
-brandSchema.pre('save', function (next) {
-    this.slug = slugify(this.brand_name, { lower: true });
-    next();
+brandSchema.pre("save", function (next) {
+  this.slug = slugify(this.brand_name, { lower: true });
+  next();
 });
 
-const Brands = model('Brands', brandSchema);
+const Brands = model("Brands", brandSchema);
 
 module.exports = Brands;
